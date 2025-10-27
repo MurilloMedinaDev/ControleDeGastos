2ea80e5e6653e1c75dc0e70b997bcdb3c8271f6f
Murillo Medina <murillobueno@gmail.com>
2025-10-27 10:34:31 -0300

    atualização graficos e Containers categorias

--- JS/dash.js
+++ JS/dash.js
@@ -1,3 +1,16 @@
 function toggleSidebar() {
     document.querySelector('.sidebar').classList.toggle('ativa');
   }
+
+  const config = {
+    type: 'pie',
+    data: dadosDoGrafico,
+    options: {
+      responsive: true,
+      maintainAspectRatio: false, // permite redimensionar o canvas
+    },
+  };
+  
+  const ctx = document.getElementById('graficoPizza').getContext('2d');
+  new Chart(ctx, config);
+  
