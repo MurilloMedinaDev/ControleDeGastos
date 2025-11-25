f073344428517f0ac2e7664d8f330725bc8ffef3
Murillo Medina <murillobueno@gmail.com>
2025-11-25 17:36:44 -0300

    Imagem S/movi OK

--- JS/extrato.js
+++ JS/extrato.js
@@ -1,4 +1,4 @@
-// carregar e excluir movimentações
+// Função para atualizar a lista e mostrar mensagem se estiver vazia
 async function carregarTodasMovimentacoes(id_usuario) {
     try {
         const resposta = await fetch(`http://localhost:3000/movimentacoes/${id_usuario}`);
@@ -7,6 +7,17 @@ async function carregarTodasMovimentacoes(id_usuario) {
         const movimentacoes = await resposta.json();
         const lista = document.querySelector('.container-Movimentacao');
         lista.innerHTML = "";
+
+        if (movimentacoes.length === 0) {
+            const vazioDiv = document.createElement('div');
+            vazioDiv.classList.add('movimentacoes-vazias');
+            vazioDiv.innerHTML = `
+                <p>Nenhuma movimentação encontrada.</p>
+                <img  src="../logo2.png" alt="Nenhuma movimentação" style="width:150px; margin-top:10px;">
+            `;
+            lista.appendChild(vazioDiv);
+            return;
+        }
 
         movimentacoes.forEach(item => {
             const div = document.createElement('div');
@@ -42,12 +53,6 @@ const idUsuario = localStorage.getItem("id_usuario");
 if (idUsuario) carregarTodasMovimentacoes(idUsuario);
 else console.error("ID do usuário não encontrado no localStorage.");
 
-
-
-
-
-
-
 // função excluir
 document.addEventListener("click", async function(e) {
     if (e.target.classList.contains("btn-excluir")) {
@@ -61,8 +66,10 @@ document.addEventListener("click", async function(e) {
             const resposta = await fetch(`http://localhost:3000/movimentacao/${idMov}`, { method: "DELETE" });
             if (!resposta.ok) throw new Error("Erro ao excluir");
 
-            div.remove(); // função para remover da tela
+            div.remove(); // remove da tela
             console.log("Movimentação excluída:", idMov);
+
+
         } catch (erro) {
             console.error("Erro ao deletar:", erro);
         }
